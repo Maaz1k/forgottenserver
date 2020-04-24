da430622ec9fa76062a192cdb8c48c73f513b093
Dejan Vuruna <dejanvuruna1@outlook.com>
2020-04-24 22:50:23 +0200

    eff

--- data/events/scripts/effects.lua
+++ data/events/scripts/effects.lua
@@ -1,6 +1,16 @@
-local position = {x=1048,y=1010, z=6, stackpos=1}
-local text = {"Tiaptra", "Tiaptra", "Tiaptra"}
+local config = {
+positions = {
 
-function onThink(interval, lastExecution)
-        return doSendAnimatedText(position,text[math.random(1,#text)], math.random(10,66 ) )
+["Tiaptra"] = { x = 1048, y = 1010, z = 6 },
+["Txt2"] = { x = 1000, y = 995, z = 6 },
+["Txt3"] = { x = 1005, y = 1003, z = 5 }
+}
+}
+
+function onThink(cid, interval, lastExecution)
+for text, pos in pairs(config.positions) do
+doSendAnimatedText(pos, text, math.random(1, 255))
 end
+
+return TRUE
+end 
