f3dccaec8ca3e3845893bd23c740fc7230341400
Dejan Vuruna <dejanvuruna1@outlook.com>
2020-04-25 21:18:35 +0200

    text

---
+++ data/globalevents/scripts/text.lua
@@ -0,0 +1,18 @@
+local effects = {
+    {position = Position(1048, 1010, 6), text = 'Teleports', effect = CONST_ME_GROUNDSHAKER},
+}
+
+function onThink(interval)
+    for i = 1, #effects do
+        local settings = effects[i]
+        local spectators = Game.getSpectators(settings.position, false, true, 7, 7, 5, 5)
+        if #spectators > 0 then
+            if settings.text then
+                for i = 1, #spectators do
+                    spectators[i]:say(settings.text, TALKTYPE_MONSTER_SAY, false, spectators[i], settings.position)
+                end
+            end
+            if settings.effect then
+                settings.position:sendMagicEffect(settings.effect)
+            end
+        end
