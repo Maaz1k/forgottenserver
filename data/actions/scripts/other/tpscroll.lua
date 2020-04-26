c07c5b90db94351b5496de22dbb420a4206aae88
Dejan Vuruna <dejanvuruna1@outlook.com>
2020-04-26 18:51:05 +0200

    tpscroll

---
+++ data/actions/scripts/other/tpscroll.lua
@@ -0,0 +1,12 @@
+function onUse(cid, item, frompos, item2, topos)
+ppos = getPlayerPosition(cid)
+temple = getPlayerMasterPos(cid)
+if (getCreatureCondition(cid, CONDITION_INFIGHT) == FALSE) then
+doTeleportThing(cid, temple, TRUE)
+doSendMagicEffect(ppos,66)
+doSendAnimatedText(frompos,'Wiee!',16)
+else
+doPlayerSendCancel(cid,"You can't teleport immediately after fight.")
+doSendMagicEffect(ppos,2)
+end
+return 1
