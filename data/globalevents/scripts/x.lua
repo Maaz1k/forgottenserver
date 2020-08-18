a9df2d7e37196cb24e9c4eae773e7ad2bb80c32f
Dejan Vuruna <dejanvuruna1@outlook.com>
2020-08-18 22:33:00 +0200

    animatedtexttest

---
+++ data/globalevents/scripts/x.lua
@@ -0,0 +1,25 @@
+function Position.sendMessage(self, message, talktype)
+    local specs = Game.getSpectators(self, false, true, 7, 7, 5, 5)
+    if #specs > 0 then
+        for i = 1, #specs do
+            local player = specs[i]
+            player:say(message, talktype or TALKTYPE_MONSTER_SAY, false, player, self)
+        end
+    end
+end
+
+
+
+local effects = {
+    {pos = Position(1398, 1008, 7), magicEffect = CONST_ME_MAGICXXXXXXX, text = "Tiaptra"}
+}
+
+function onThink()
+    for , v in pairs(effects) do
+        v.pos:sendMagicEffect(magicEffect)
+        if v.text ~= "" then
+            v.pos:sendMessage(v.text)
+        end
+    end
+    return true
+end
