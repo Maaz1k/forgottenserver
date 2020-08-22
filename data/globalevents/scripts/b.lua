e88a49bc2a47a0afd9fc99ee1772e2be6834626e
Dejan Vuruna <dejanvuruna1@outlook.com>
2020-08-22 02:45:02 +0200

    templeanimations

--- data/globalevents/scripts/b.lua
+++ data/globalevents/scripts/b.lua
@@ -1,5 +1,5 @@
 local effects = {
-    {pos = Position(1393, 1006, 7), animation = CONST_ME_FIREWORK_RED, text = ""},
+    {pos = Position(1393, 1010, 7), animation = CONST_ME_BLOCKHIT, text = ""},
 }
 
 function onThink()
