63328db15cf72eb2e5bc84c0a8191404522e2425
Dejan Vuruna <dejanvuruna1@outlook.com>
2020-08-23 20:47:37 +0200

    update

---
+++ data/globalevents/scripts/trainers.lua
@@ -0,0 +1,19 @@
+local effects = {
+    {pos = Position(1393, 1010, 7), animation = CONST_ME_POFF, text = ""},
+}
+
+function onThink()
+    for _, effect in pairs(effects) do
+        if effect.animation and effect.animation ~= CONST_ME_NONE then
+            effect.pos:sendMagicEffect(effect.animation)
+        end
+
+        if effect.text and effect.text ~= "" then
+            for _, spec in pairs(Game.getSpectators(effect.pos, false, true, 7, 7, 5, 5)) do
+                spec:say(effect.text, TALKTYPE_MONSTER_SAY, false, nil, effect.pos)
+                break
+            end
+        end
+    end
+    return true
+end
